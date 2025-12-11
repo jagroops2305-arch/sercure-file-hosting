e73d4cde0d585f7dd6736b3f83f933eb8f79e2f1
Jagroop Singh <jagroops2305@gmail.com>
2025-12-10 21:00:48 -0500

    Added start and dev scripts to backend package.json.

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//define routes for testing
+app.get('/', (req, res) => {
+    res.send('Secure File Hosting Backend is running');
+});
+
+//start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
